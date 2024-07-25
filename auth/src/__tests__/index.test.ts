09e6dddc29f0190c48316efd11433559747cedcf
John <johnolshea@gmail.com>
2024-07-26 00:14:09 +0100

    £Feat: First test

--- auth/src/__tests__/index.test.ts
+++ auth/src/__tests__/index.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import app from '../app';
 
-it('Responds with status 200', async () => {
+it('Responds with a status of 200', async () => {
   await request(app).get('/').expect(200);
 });
