db3060398e89d0c9f555ec03843f8353f08c39d4
John <johnolshea@gmail.com>
2024-08-04 07:10:57 +0100

    refactor(auth): improve error in user model and signup route

--- auth/src/models/__tests__/user.test.ts
+++ auth/src/models/__tests__/user.test.ts
@@ -1,4 +1,6 @@
 import User from '../user';
+import BaseCustomError from '../../errors/base-custom-error';
+import DuplicatedEmail from '../../errors/duplicated-email';
 
 it('should not save a new user if the email already in the database', async () => {
   const userInfo = {
@@ -10,13 +12,19 @@ it('should not save a new user if the email already in the database', async () =
   expect(newUser1).toBeDefined();
   expect(newUser1.email).toEqual(userInfo.email);
 
-  let err;
+  let err: DuplicatedEmail | undefined;
   try {
     await User.create(userInfo);
   } catch (e) {
     err = e;
   }
 
+  const serializedErrorOutput = err ? err.serializeErrorOutput() : undefined;
+
   expect(err).toBeDefined();
-  expect(err.message).toEqual('email is already in the database');
+  expect(err).toBeInstanceOf(BaseCustomError);
+  expect(serializedErrorOutput).toBeDefined();
+  expect(serializedErrorOutput?.errors[0].message).toEqual(
+    'The email is already in the database'
+  );
 });
