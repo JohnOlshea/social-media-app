f8da0da77ee4272386c8ad63754ae6eaf0df0f08
John <johnolshea@gmail.com>
2024-08-11 12:00:38 +0100

    feat(auth): added mock api to send email with test

--- auth/src/utils/__tests__/email-sender.test.ts
+++ auth/src/utils/__tests__/email-sender.test.ts
@@ -1,4 +1,5 @@
 import { EmailSender } from '../index';
+import { MockEmailApi } from '../../test-utils/mock-email-api';
 
 it('should throw an error when sending an email if the EmailSender is deactivated', async () => {
   const emailSender = EmailSender.getInstance();
@@ -6,4 +7,23 @@ it('should throw an error when sending an email if the EmailSender is deactivate
   await expect(
     emailSender.sendEmail({ toEmail: 'test@test.com' })
   ).rejects.toThrowError('EmailSender is not active');
+});
+
+it('should throw and error when sending an email if the api is not set', async () => {
+  const emailSender = EmailSender.getInstance();
+  emailSender.activate();
+  await expect(
+    emailSender.sendEmail({ toEmail: 'test@test.com' })
+  ).rejects.toThrowError('EmailApi is not set');
+});
+
+it('should send the email correctly if the EmailSender is active and the EmailApi set', async () => {
+  const emailSender = EmailSender.getInstance();
+  const mockEmailApi = new MockEmailApi();
+
+  emailSender.activate();
+  emailSender.setEmailApi(mockEmailApi);
+
+  const res = await emailSender.sendEmail({ toEmail: 'test@test.com' });
+  expect(res.toEmail).toEqual('test@test.com');
 });
--- auth/src/utils/email-sender.ts
+++ auth/src/utils/email-sender.ts
@@ -1,15 +1,24 @@
 // Singleton pattern
 
-export type EmalSenderSendEmailArgs = {
+export type EmailApiSendEmailArgs = {
   toEmail: string;
 };
 
-export type EmailSenderSendEmailResponse = {
+export type EmailApiSendEmailResponse = {
   toEmail: string;
   status: 'success' | 'error';
 };
-export default class EmailSender {
+
+export interface EmailSenderEmailApi {
+  sendEmail: (
+    args: EmailApiSendEmailArgs
+  ) => Promise<EmailApiSendEmailResponse>;
+}
+
+export default class EmailSender implements EmailSenderEmailApi {
   private isActive = false;
+
+  private emailApi: EmailSenderEmailApi | undefined;
 
   private static emailSenderInstance: EmailSender;
 
@@ -25,23 +34,33 @@ export default class EmailSender {
     return this.emailSenderInstance;
   }
 
+  activate(): void {
+    this.isActive = true;
+  }
+
   deactivate(): void {
     this.isActive = false;
   }
 
+  setEmailApi(emailApi: EmailSenderEmailApi): void {
+    this.emailApi = emailApi;
+  }
+
   async sendEmail(
-    args: EmalSenderSendEmailArgs
-  ): Promise<EmailSenderSendEmailResponse> {
+    args: EmailApiSendEmailArgs
+  ): Promise<EmailApiSendEmailResponse> {
     this.validateEmailSender();
 
-    return new Promise((resolve) =>
-      resolve({ toEmail: args.toEmail, status: 'success' })
-    );
+    return this.emailApi!.sendEmail(args);
   }
 
   private validateEmailSender(): void {
     if (!this.isActive) {
       throw new Error('EmailSender is not active');
+    }
+
+    if (!this.emailApi) {
+      throw new Error('EmailApi is not set');
     }
   }
 }
