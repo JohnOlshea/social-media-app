bdd4db60542e0215fa36eba77f33294b020a2f7d
John <johnolshea@gmail.com>
2024-08-11 07:27:39 +0100

    Auth: create test for user signup

--- auth/src/models/__tests__/user.test.ts
+++ auth/src/models/__tests__/user.test.ts
@@ -1,30 +1,60 @@
+import { randomBytes } from 'crypto';
 import User from '../user';
 import BaseCustomError from '../../errors/base-custom-error';
 import DuplicatedEmail from '../../errors/duplicated-email';
+import { PasswordHash } from '../../utils';
 
-it('should not save a new user if the email already in the database', async () => {
+describe('tests the User mongoose model', () => {
   const userInfo = {
     email: 'test@test.com',
     password: 'Valid123',
   };
 
-  const newUser1 = await User.create(userInfo);
-  expect(newUser1).toBeDefined();
-  expect(newUser1.email).toEqual(userInfo.email);
+  it('should not save a new user if the email already in the database', async () => {
+    const newUser1 = await User.create(userInfo);
+    expect(newUser1).toBeDefined();
+    expect(newUser1.email).toEqual(userInfo.email);
 
-  let err: DuplicatedEmail | undefined;
-  try {
-    await User.create(userInfo);
-  } catch (e) {
-    err = e;
-  }
+    let err: DuplicatedEmail | undefined;
+    try {
+      await User.create(userInfo);
+    } catch (e) {
+      err = e;
+    }
 
-  const serializedErrorOutput = err ? err.serializeErrorOutput() : undefined;
+    const serializedErrorOutput = err ? err.serializeErrorOutput() : undefined;
 
-  expect(err).toBeDefined();
-  expect(err).toBeInstanceOf(BaseCustomError);
-  expect(serializedErrorOutput).toBeDefined();
-  expect(serializedErrorOutput?.errors[0].message).toEqual(
-    'The email is already in the database'
-  );
+    expect(err).toBeDefined();
+    expect(err).toBeInstanceOf(BaseCustomError);
+    expect(serializedErrorOutput).toBeDefined();
+    expect(serializedErrorOutput?.errors[0].message).toEqual(
+      'The email is already in the database'
+    );
+  });
+
+  it('should encrypt the password when creating the user', async () => {
+    const newUser = await User.create(userInfo);
+    expect(newUser.password).not.toEqual(userInfo.password);
+    expect(newUser.password.split('.')).toHaveLength(2);
+    expect(newUser.password.split('.')[1].length).toEqual(
+      randomBytes(16).toString('hex').length
+    );
+  });
+
+  it('should return true when comparing the hashedPassword with its original providedPassword', async () => {
+    const newUser = await User.create(userInfo);
+
+    expect(
+      PasswordHash.compareSync({
+        providedPassword: '1234',
+        storedPassword: newUser.password,
+      })
+    ).toEqual(false);
+    expect(
+      PasswordHash.compareSync({
+        providedPassword: userInfo.password,
+        storedPassword: newUser.password,
+      })
+    ).toEqual(true);
+  });
 });
